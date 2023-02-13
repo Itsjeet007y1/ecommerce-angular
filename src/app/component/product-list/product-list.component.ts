60e09f825b33c379a616393da76ee099a9f14fe5
Itsjeet007y1 <jeet.62.jp@gmail.com>
2023-02-13 23:48:08 +0530

    Added code to get products by category, using angular routing

--- src/app/component/product-list/product-list.component.ts
+++ src/app/component/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Product } from 'src/app/common/product';
 import { ProductService } from 'src/app/services/product.service';
 
@@ -10,17 +11,33 @@ import { ProductService } from 'src/app/services/product.service';
 export class ProductListComponent {
 
   products: Product[] = []
+  currentCategoryId: number = 1;
 
-  constructor(private productService: ProductService) {
-
-  }
+  constructor(private productService: ProductService,
+              private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.listProducts();
+    this.route.paramMap.subscribe(() => {
+      this.listProducts();
+    });
   }
   
   listProducts() {
-    this.productService.getProductList().subscribe(
+
+    // check if id parameter is available
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+
+    if(hasCategoryId) {
+      // get the param id and convert it to number using '+' symbol
+      // here we have used '!' sign after get('id'), this is non-null assertion operator
+      // that tells the compiler that the object is not null
+      this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;
+    } else {
+      // not category id available.. default category id is 1
+      this.currentCategoryId = 1;
+    }
+
+    this.productService.getProductList(this.currentCategoryId).subscribe(
       data => {
         this.products = data;
       }
