1c20ecd2a6e509cf98a45072773883e5041eefed
Itsjeet007y1 <jeet.62.jp@gmail.com>
2023-02-25 17:25:28 +0530

    Security: Added self signed certificate

--- src/app/config/my-app-config.ts
+++ src/app/config/my-app-config.ts
@@ -3,7 +3,7 @@ export default {
     oidc: {
         clientId: '0oa8equkkhAQffzSf5d7',
         issuer: 'https://dev-44067168.okta.com/oauth2/default',
-        redirectUri: 'http://localhost:4200/login/callback',
+        redirectUri: 'https://localhost:4200/login/callback',
         scopes: ['openid', 'profile', 'email']
     }
     
